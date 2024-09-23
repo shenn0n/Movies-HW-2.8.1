e28de7853644b335c81bc619c6a5c7d0e195d716
shenn0n <shenn0n4412@gmail.com>
2024-09-23 21:21:45 +0300

    Initial Commit

---
+++ Movies HW 2.8.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies HW 2.8.1
+//
+//  Created by Александр Манжосов on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
